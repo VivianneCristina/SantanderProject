884611d5deb2b9208a10a8f128a2071d2952209e
Vivianne Cristina <vivianne.crisns@gmail.com>
2018-08-01 16:01:56 -0300

    tela de investimento em andamento

--- app/src/main/java/com/viviannecristina/santanderproject/MainActivity.java
+++ app/src/main/java/com/viviannecristina/santanderproject/MainActivity.java
@@ -5,13 +5,12 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
 import com.viviannecristina.santanderproject.fragment.ContatoFragment;
 import com.viviannecristina.santanderproject.fragment.InvestimentoFragment;
-import com.viviannecristina.santanderproject.utils.PageAnimation;
+import com.viviannecristina.santanderproject.config.PageAnimation;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/viviannecristina/santanderproject/fragment/InvestimentoFragment.java
+++ app/src/main/java/com/viviannecristina/santanderproject/fragment/InvestimentoFragment.java
@@ -1,7 +1,6 @@
 package com.viviannecristina.santanderproject.fragment;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -9,9 +8,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.viviannecristina.santanderproject.R;
+import com.viviannecristina.santanderproject.domain.SubScreen;
+import com.viviannecristina.santanderproject.config.IRetrofitConfig;
+import com.viviannecristina.santanderproject.config.Routers;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,7 +28,9 @@ import com.viviannecristina.santanderproject.R;
 public class InvestimentoFragment extends Fragment {
 
     private Toolbar toolbar;
-    private TextView title;
+
+    private TextView title,fundName, whatIs, definition, riskTitle, infoTitle;
+    private Button button;
 
     public InvestimentoFragment() {
         // Required empty public constructor
@@ -30,17 +41,52 @@ public class InvestimentoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+
         View view = inflater.inflate(R.layout.fragment_investimento, container, false);
-        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
-        TextView text = (TextView) view.findViewById(R.id.title);
+        toolbar = view.findViewById(R.id.toolbar);
+        title =  view.findViewById(R.id.tv_title);
+        whatIs = view.findViewById(R.id.tv_what_is);
+        fundName = view.findViewById(R.id.tv_fund_name);
+        definition = view.findViewById(R.id.tv_definition);
+        riskTitle = view.findViewById(R.id.tv_risk_title);
+        infoTitle = view.findViewById(R.id.tv_info_title);
+
 
 
         AppCompatActivity activity = (AppCompatActivity) getActivity();
         activity.setSupportActionBar(toolbar);
         activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        init();
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_investimento, container, false);
+
     }
+    private void init(){
+
+        Routers routers = IRetrofitConfig.retrofit.create(Routers.class);
+
+        final Call<SubScreen> subScreen = routers.getSubScreen();
+
+        subScreen.enqueue(new Callback<SubScreen>() {
+            @Override
+            public void onResponse(Call<SubScreen> call, Response<SubScreen> response) {
+                SubScreen subScreenReturn = response.body();
+
+                title.setText(subScreenReturn.getScreen().getTitle());
+                fundName.setText(subScreenReturn.getScreen().getFundName());
+                whatIs.setText(subScreenReturn.getScreen().getWhatIs());
+                definition.setText(subScreenReturn.getScreen().getDefinition());
+                riskTitle.setText(subScreenReturn.getScreen().getRiskTitle());
+                infoTitle.setText(subScreenReturn.getScreen().getTitle());
+            }
+
+            @Override
+            public void onFailure(Call<SubScreen> call, Throwable t) {
+
+            }
+        });
+    }
+
 }
